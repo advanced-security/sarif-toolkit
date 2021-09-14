596fed3e03a4d0a65b65ba4e321c50072004522e
Mathew Payne <geekmasher@gmail.com>
2021-09-14 13:19:45 +0100

    Add SARIF Class and utils

---
+++ sariftoolkit/sarif/sarif.py
@@ -0,0 +1,36 @@
+import os
+import json
+from dataclasses import dataclass, field
+import logging
+from typing import List, Any
+
+from sariftoolkit.utils.dataclasses import _dataclass_from_dict
+from sariftoolkit.sarif.models import *
+
+
+def loadSarif(path: str):
+    path = os.path.abspath(path)
+    print(f"Loading SARIF File: '{path}'")
+    with open(path, "r") as handle:
+        sarif_dict = json.load(handle)
+
+    return _dataclass_from_dict(SarifModel, sarif_dict)
+
+
+def exportSarif(path: str, sarif: SarifModel):
+    def _exportAsDict(obj):
+        data = asdict(obj)
+
+        if hasattr(obj, "__holders__"):
+            for key, value in obj.__holders__.items():
+                if value in data:
+                    data[key] = data[value]
+                    data.pop(value)
+
+        return data
+
+    path = os.path.abspath(path)
+    print(f"Exporting SARIF File: '{path}'")
+
+    with open(path, "w") as handle:
+        json.dump(_exportAsDict(sarif), handle, indent=4)
---
+++ sariftoolkit/utils/dataclasses.py
@@ -0,0 +1,26 @@
+import logging
+
+
+def _dataclass_from_dict(klass, dikt):
+    try:
+        fieldtypes = klass.__annotations__
+        data = {}
+        for f in dikt:
+            if f in fieldtypes:
+                data[f] = _dataclass_from_dict(fieldtypes[f], dikt[f])
+            else:
+                # print(f"Ignore value: {f} ({klass})")
+                holder = klass.__holders__.get(f)
+                if holder:
+                    data[holder] = _dataclass_from_dict(klass.__holders__[f], dikt[f])
+
+        # return klass(**{f: _dataclass_from_dict(fieldtypes[f], dikt[f]) })
+        return klass(**data)
+
+    except KeyError as err:
+        raise err
+
+    except AttributeError as err:
+        if isinstance(dikt, (tuple, list)):
+            return [_dataclass_from_dict(klass.__args__[0], f) for f in dikt]
+        return dikt
