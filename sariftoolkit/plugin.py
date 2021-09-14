eb2753628dd2beffddbfa105404e6a03879a91c7
Mathew Payne <geekmasher@gmail.com>
2021-09-14 13:20:10 +0100

    Add better plugin support

--- sariftoolkit/plugin.py
+++ sariftoolkit/plugin.py
@@ -1,16 +1,24 @@
+import os
+import json
 import logging
 from argparse import ArgumentParser
 from dataclasses import dataclass
+from typing import Tuple
 
 from sariftoolkit.config import Plugins
+from sariftoolkit.sarif.sarif import loadSarif
 
 
 def _dynamic_import(path: str, class_name: str):
     paths = path.split(".")
-    module = __import__(paths[0])
-    for p in paths[1:]:
-        module = getattr(module, p)
-    return getattr(module, class_name)
+    try:
+        module = __import__(paths[0])
+        for p in paths[1:]:
+            module = getattr(module, p)
+        return getattr(module, class_name)
+
+    except Exception as err:
+        logging.error(f"Failed to import {path}")
 
 
 def loadPlugins():
@@ -47,3 +55,30 @@ class Plugin:
 
     def run(self, **kargvs):
         raise Exception("Plugin Sub Class doesn't support a run function...")
+
+    def loadSarif(self, path: str):
+        if not os.path.exists(path):
+            raise Exception(f"{path} doesn't exist")
+
+        sarif_files = []
+
+        if os.path.isdir(path):
+            for file in os.listdir(path):
+                file_path = os.path.abspath(os.path.join(path, file))
+                _, extention = os.path.splitext(file)
+
+                if extention in [".json", ".sarif"]:
+
+                    sarif_model = loadSarif(file_path)
+
+                    sarif_files.append((sarif_model, file_path))
+        else:
+
+            _, extention = os.path.splitext(path)
+            if extention in [".json", ".sarif"]:
+
+                sarif_model = loadSarif(path)
+
+                sarif_files.append((sarif_model, path))
+
+        return sarif_files
