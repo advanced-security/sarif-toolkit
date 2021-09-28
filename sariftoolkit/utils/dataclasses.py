aa3baa46c08f951abf87a5a004e4d96109d6e7c9
Mathew Payne <geekmasher@gmail.com>
2021-09-28 16:01:05 +0100

    Add warning

--- sariftoolkit/utils/dataclasses.py
+++ sariftoolkit/utils/dataclasses.py
@@ -21,6 +21,7 @@ def _dataclass_from_dict(klass, dikt):
         raise err
 
     except AttributeError as err:
+        logging.warning(f"AttributeError: {err}")
         if isinstance(dikt, (tuple, list)):
             return [_dataclass_from_dict(klass.__args__[0], f) for f in dikt]
         return dikt
